60d5f3a260f804ecf6835522692da15d90ca9a2a
Fantomext <Fantomext80@gmail.com>
2023-06-10 13:01:08 +0300

    StartFix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
+    private void Start()
+    {
+        Cursor.visible = true;
+
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
